75776288ecb42cd6ec98081f8be4ae371b7db06a
Etienne from Maison <etienne_jean5@hotmail.fr>
2018-09-16 02:21:13 +0200

    fin du dvpt git statusgit status ajout de dvpt5

--- genome_generation.py
+++ genome_generation.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-from Bio.Seq import *
 import random as rd
 
 
 
-class Genome(Seq):
-    def __init__(self, sequence, circular=True):
-        Seq.__init__(self, sequence)
-        self.circular=circular
-        
-    def show(self):
-        """Prints the attributes and values of the instance"""
-        for key, value in self.__dict__.items():
-            print('{:30s}{}'.format(key, value))
+
+
+
+
+
 
 
 
--- sequencing.py
+++ sequencing.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from Bio.Seq import *
 import random as rd
 
 
